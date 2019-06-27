0f97020a0d8b9436b3508fb0c0455806799be759
teruya <abas.trace.on@gmail.com>
2019-06-27 10:01:20 +0900

    knap

--- knap_memo.py
+++ knap_memo.py
@@ -4,37 +4,24 @@ input = sys.stdin.readline
 N = 0
 W = 0
 items = []
-dp = []
 def main():
-	global N, W, items, dp
+	global N, W, items
 	N, W = map(int, input().strip().split())
-	items = []
 	for i in range(N):
 		v, w = map(int, input().strip().split())
 		items.append((v, w))
-	dp = [[-1] * (W+1) for _ in range(N+1)]
-	rec(0, W)
-	print(dp[0][W])
+	print(val(0, W, 0))
 
-# spaceのカバンに商品i以降を入れる時の最大値
-def rec(i, space):
-	if dp[i][space] != -1:
-		return dp[i][space]
 
-	if i >= N:
-		dp[i][space] = 0
-		return dp[i][space]
+def val(i, space, value):
+	if i > N-1:
+		return value
 
 	if space - items[i][1] < 0:
-		dp[i][space] = rec(i+1, space)
-		return dp[i][space]
+		return val(i+1, space, value)
 
-	dp[i][space] = max(
-		rec(i+1, space - items[i][1]) + items[i][0],
-		rec(i+1, space)
-	)
-
-	return dp[i][space]
+	return max(val(i+1, space - items[i][1], value + items[i][0]), val(i+1, space, value))
+	# max(take -> -1, not take -> -1)
 
 if __name__ == '__main__':
 	main()
