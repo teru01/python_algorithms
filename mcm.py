f62bf2c638eaf789b7f17c265ff550842cce93d9
teruya <abas.trace.on@gmail.com>
2019-06-29 10:13:30 +0900

    redundunt mcm

--- mcm.py
+++ mcm.py
@@ -1,24 +1,25 @@
 import sys
 input = sys.stdin.readline
-N = 100
+a = []
+
+#l <= x < rまでの行列せきの最小値
+def rec(l, r):
+	if l+1 == r:
+		return 0
+	if l+2 == r:
+		return a[l][0] * a[l][1] * a[l+1][1]
+	m = 100000000
+	for i in range(l+1, r):
+		m = min(m, rec(l, i) + rec(i, r) + a[l][0] * a[i][0] * a[r-1][1])
+	return m
+
 def main():
+	global a
 	n = int(input().strip())
-	p = [0] * (N+1)
-	for i in range(1, n+1):
-		p[i-1], p[i] = map(int, input().strip().split())
-	m = [[0] * (N+1) for i in range(N+1)]
-	for l in range(2, n+1):
-		# i, jは対象となる行列の数が2, 3, 4...と増えていくようにする
-		# ((M1,M2),M3)などは(M1,M2)が求まっている必要がある
-		for i in range(1, n-l+2):
-			# print("i: {}".format(i))
-			j = i + l - 1 # lはウィンドウの長さ。
-			# print("j: {}".format(j))
-			# print("--------------------")
-			m[i][j] = 100000000
-			for k in range(i, j):
-				m[i][j] = min(m[i][j], m[i][k] + m[k+1][j] + p[i-1] * p[k] * p[j])
-	print(m[1][n])
+	for i in range(n):
+		a.append(tuple(map(int, input().strip().split())))
+
+	print(rec(0, n))
 
 if __name__ == '__main__':
 	main()
